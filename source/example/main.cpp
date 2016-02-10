ef261ff2309529db88b12bd6cefa74bb2b1640d3
Daniel Grondin <tm.kkitsune@gmail.com>
2016-02-10 09:30:38 -0500

    Found the issue : CLion doesn't show cerr text, and the app could not find its fonts

--- source/example/main.cpp
+++ source/example/main.cpp
@@ -39,7 +39,7 @@ int main(int /*argc*/, char* /*argv*/[])
 	}
 	catch(exception& e)
 	{
-		cerr << "Exception caught : " << e.what() << endl;
+		cout << "Exception caught : " << e.what() << endl;
 	}
 
 	Core::destroy();
