047015398f606a1793861c54bf251506d076a9f3
Daniel Grondin <tm.kkitsune@gmail.com>
2016-02-09 23:51:32 -0500

    Added a simple example Screen but the app closes unexpectedly (with exit code 0???) before running the "ex->visible(true)"

--- source/example/main.cpp
+++ source/example/main.cpp
@@ -5,13 +5,37 @@
 
 using namespace std;
 
+class ExampleScreen : public Screen
+{
+public:
+	ExampleScreen() : Screen(ivec2(960, 640), "GoblinEngine Example")
+	{ }
+
+	virtual ~ExampleScreen()
+	{ }
+
+
+	virtual bool keyboard_event(Key code, int, Action action, int) override
+	{
+		if(code == Key::Escape && action == Action::Press)
+		{
+			should_close(true);
+			return true;
+		}
+
+		return false;
+	}
+};
+
 int main(int /*argc*/, char* /*argv*/[])
 {
 	Core::create();
 
 	try
 	{
-		Screen wnd(ivec2(960, 640), "GoblinEngine Example");
+		Ref<ExampleScreen> ex(new ExampleScreen);
+		ex->visible(true);
+		Core::instance().run();
 	}
 	catch(exception& e)
 	{
