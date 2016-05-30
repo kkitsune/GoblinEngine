6c69ee824aafc10e47512b77473d927a2b6fd7b3
Daniel Grondin <tm.kkitsune@gmail.com>
2016-05-30 16:46:51 -0400

    Initializing Blendish

--- engine/source/Blendish.cpp
+++ engine/source/Blendish.cpp
@@ -4,12 +4,15 @@
 #pragma GCC diagnostic ignored "-Wpedantic"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #define BLENDISH_IMPLEMENTATION
+#include "deps/nanovg/blender_icons.h"
 #include "deps/nanovg/nanovg.h"
 #include "deps/nanovg/blendish.h"
 #pragma GCC diagnostic pop
 
 Blendish::Blendish(Painter& painter) : _vg(painter._vg)
 {
+	bndSetIconImage(painter.create_image(blender_icons, sizeof(blender_icons), ImageFlags::None));
+	bndSetFont(painter.create_font("dejavu", "data/DejaVuSans.ttf"));
 }
 
 Blendish::~Blendish()
