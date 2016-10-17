39f97e66aa6f9342e9017c0acb0a31c1ca6ef5e2
Daniel Grondin <tm.kkitsune@gmail.com>
2016-10-17 01:34:44 -0400

    Squashed 'engine/libs/bgfx/' content from commit 7fc69ce
    git-subtree-dir: engine/libs/bgfx
    git-subtree-split: 7fc69cebfcec04bdb90945baf3769ca212c435ad

---
+++ cmake/bx.cmake
@@ -0,0 +1,31 @@
+# bgfx.cmake - bgfx building in cmake
+# Written in 2016 by Joshua Brookover <josh@jalb.me>
+
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
+
+# You should have received a copy of the CC0 Public Domain Dedication along with
+# this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+# Ensure the directory exists
+if( NOT IS_DIRECTORY ${BX_DIR} )
+	message( SEND_ERROR "Could not load bx, directory does not exist. ${BX_DIR}" )
+	return()
+endif()
+
+# Create the bx target
+add_library( bx INTERFACE )
+
+# Add include directory of bx
+target_include_directories( bx INTERFACE ${BX_DIR}/include )
+
+# Build system specific configurations
+if( MSVC )
+	target_include_directories( bx INTERFACE ${BX_DIR}/include/compat/msvc )
+	target_compile_definitions( bx INTERFACE "__STDC_FORMAT_MACROS" )
+elseif( MINGW )
+	target_include_directories( bx INTERFACE ${BX_DIR}/include/compat/mingw )
+elseif( APPLE )
+	target_include_directories( bx INTERFACE ${BX_DIR}/include/compat/osx )
+endif()
